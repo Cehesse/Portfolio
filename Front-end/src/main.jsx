83ecbcd70591de5f75549bc713e099e50f730720
Cehesse <chatellet.steven@gmail.com>
2023-10-10 15:39:52 +0200

    Routes

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,8 +1,7 @@
 //REACT
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //SASS
 import "./sass/main.scss";
@@ -22,17 +21,17 @@ import Footer from "./layout/Footer"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/Portfolio">
+    <Router basename="/Portfolio">
       <div className="App">
         <Header />
           <Routes>
-            <Route path="/" component={<Home />} />
-            <Route path="/About" component={<About />} />
-            <Route path="/Projet/:Number" component={<Projet />} />
-            <Route path="*" component={<Error />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Projet/:Number" element={<Projet />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         <Footer />
       </div>
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
 );
