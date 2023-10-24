5698b3c57181a1c88aea69b108750f6d9b2490c7
Cehesse <chatellet.steven@gmail.com>
2023-10-24 20:48:14 +0200

    Router

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,26 +1,8 @@
-/* import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Portfolio",
   plugins: [react()],
-}) */
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import VitePluginGhPages from 'vite-plugin-gh-pages';
-
-// ...
-
-export default defineConfig({
-
-    base: "/Portfolio",
-    plugins: [
-    react(),
-    reactRefresh(),
-    VitePluginGhPages(), // Ajoutez cette ligne
-  ],
-  // ...
-});
+})
