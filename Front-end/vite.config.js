36ebee0f4c7eaa3d18ce9aaa47361a3528ba93ad
Cehesse <chatellet.steven@gmail.com>
2023-10-27 11:09:12 +0200

    Test AWS

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Portfolio",
   plugins: [react()],
-  /*   resolve: {
+  resolve: {
     alias: {
       "./runtimeConfig": "./runtimeConfig.browser",
     },
-  }, */
+  },
 });
